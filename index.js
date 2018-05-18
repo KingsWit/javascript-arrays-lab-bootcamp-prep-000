78a6b0c4f56264b58e8d4df843cd6743b0fe91c6
Taylor Tellman <taylortellman@gmail.com>
2018-05-18 20:32:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,4 +3,6 @@ function destructivelyAppendKitten(name){
   return kittens}
 function destructivelyPrependKitten(name){
   kittens.unshift(name);}
-  
+function destructivelyRemoveLastKitten(){
+  kittens.pop();
+}
